a52d1fc66c86d99d307ad2afe31423d51919f0c0
JehanKandy <jehankandy@gmail.com>
2023-05-04 20:29:48 +0530

    Update

--- src/javacurrencycalculator/JavaCurrencyCalculator.java
+++ src/javacurrencycalculator/JavaCurrencyCalculator.java
@@ -17,6 +17,8 @@ public class JavaCurrencyCalculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
     }
     
 }
